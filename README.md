de7e35dc053a3a2ba2a987e0e7ee853c826e3369
fluxstride <adekoyasamuel52@gmail.com>
2021-04-03 00:08:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-xdfnnx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-xdfnnx)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+let fetch = require("node-fetch");
+
+let varia = new Promise((res, rej)=>{res("My Name Is adekoya samuel")} )
+
+varia
+.then(res=>console.log(res))
+
